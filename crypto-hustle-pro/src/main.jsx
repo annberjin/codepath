d6b9de52c912dd24717d0536d3ca5afd84cfc39e
Bernardo <bernardoxie@gmail.com>
2025-04-11 22:38:57 -0400

    home button

--- crypto-hustle-pro/src/main.jsx
+++ crypto-hustle-pro/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import Layout from '../routes/Layout';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index={true} element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+// Here we make the Layout element the root path and nest the other routes below that path. They will be rendered where that Outlet element was in our Layout.jsx file. Index=true make the app componenet the default child
